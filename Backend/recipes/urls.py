3f4a08e8c3e6a19632aad1d8fac3b8dc2bfbfb61
JVC0 <josephvanegas48@gmail.com>
2025-04-02 13:45:13 +0200

    Fixes #17:Created views for recipes

--- Backend/recipes/urls.py
+++ Backend/recipes/urls.py
@@ -6,5 +6,8 @@ app_name = "recipes"
 
 urlpatterns = [
     path("", views.recipe_list, name="recipe-list"),
+    path("add/", views.add_recipe, name="add-recipe"),
+    path("delete/", views.delete_recipe, name="del-recipe"),
+    path("edit/", views.edit_recipe, name="edit-recipe"),
     path("<slug:recipe_slug>/", views.recipe_detail, name="recipe-detail"),
 ]
--- Backend/recipes/views.py
+++ Backend/recipes/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Recipe
 from .serializers import RecipeSerializer
+import json
+from django.http import JsonResponse
 
 
-# Create your views here.
 def recipe_list(request):
     recipes = Recipe.objects.all()
     serializer = RecipeSerializer(recipes, request=request)
@@ -14,3 +15,29 @@ def recipe_detail(request, recipe_slug):
     recipes = Recipe.objects.get(slug=recipe_slug)
     serializer = RecipeSerializer(recipes, request=request)
     return serializer.json_response()
+
+
+def add_recipe(request):
+    data = json.loads(request.body)
+    description = data.get("description")
+    name = data.get("name")
+    product_group = data.get("product_group")
+    user = request.user
+    recipe = Recipe.objects.create(
+        name=name, description=description, product_group=product_group, user=user
+    )
+    recipe.save()
+    return JsonResponse({"message": "Recipe was added."})
+
+
+def delete_recipe(request):
+    data = json.loads(request.body)
+    name = data.get("name")
+    recipe = Recipe.objects.get(name=name)
+    recipe.delete()
+    return JsonResponse({"message": "Recipe was deleted."})
+
+
+def edit_recipe(request):
+    data = json.loads(request.body)
+    name = data.get("name")
