15d3f2afc12be03d83df4c859f6ddb0b7cfd00ff
JVCO <josephvanegas48@gmail.com>
2025-04-29 10:46:44 +0100

    Fixes #27:Email verification almost done

--- Backend/accounts/tasks.py
+++ Backend/accounts/tasks.py
@@ -2,27 +2,26 @@ from django.conf import settings
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django_rq import job
-from django.urls import reverse
 
 
 @job
 def send_verification_email(base_url, user):
     token = user.token.key
-    verification_link = f"{base_url}{reverse('verify-email')}?token={token}"
+    verification_link = f'{base_url}api/auth/verify-email/{token}'
 
     email_body = render_to_string(
-        "users/email_verification.html",
+        'accounts/email_verification.html',
         {
-            "user": user,
-            "verification_link": verification_link,
+            'user': user,
+            'verification_link': verification_link,
         },
     )
 
     email = EmailMessage(
-        subject="Verify Your Email Address",
+        subject='Verify Your Email Address',
         body=email_body,
         from_email=settings.DEFAULT_FROM_EMAIL,
         to=[user.email],
     )
-    email.content_subtype = "html"
+    email.content_subtype = 'html'
     email.send()
--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("login/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
     path("register/", views.user_register, name="register"),
-    path("verify-email/", views.verify_email, name="verify-email"),
+    path("verify-email/<token>", views.verify_email, name="verify-email"),
     path(
         "resend-verification/",
         views.resend_verification_email,
